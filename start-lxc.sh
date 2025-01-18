f800860269ff4f617abaae1c7509f046e327426b
Juliannars1 <julian_ars1@hotmail.com>
2025-01-18 13:40:23 -0500

    first commit 2

--- start-lxc.sh
+++ start-lxc.sh
@@ -1,30 +1,31 @@
+#!/bin/bash
 source .env
-CONTAINER_PREFIX=${CONTAINER_PREFIX}
+CONTAINER_PREFIX=${CONTAINER_PREFIX:-"myproject"}
 
-LXC_NAME_PHP_CODEIGNITER="php-codeigniter"
-LXC_NAME_PHP_WORDPRESS="php-wordpress"
-LXC_NAME_NGINX="nginx"
-LXC_NAME_MYSQL="mysql"
+LXC_NAME_PHP_CODEIGNITER="${CONTAINER_PREFIX}-php-codeigniter"
+LXC_NAME_PHP_WORDPRESS="${CONTAINER_PREFIX}-php-wordpress"
+LXC_NAME_NGINX="${CONTAINER_PREFIX}-nginx"
+LXC_NAME_MYSQL="${CONTAINER_PREFIX}-mysql"
 
-SUDO_USER="root"
+WWW_PATH="/var/www/myproject"
 DB_NAME="fiduprevisora"
 
 # Eliminar contenedores si existen
 for container in "${LXC_NAME_PHP_CODEIGNITER}" "${LXC_NAME_PHP_WORDPRESS}" "${LXC_NAME_NGINX}" "${LXC_NAME_MYSQL}"; do
-    if lxc list | grep -q "${container}"; then
+    if lxc list | grep -qw "${container}"; then
+        echo "Deleting container: ${container}"
         lxc delete "${container}" --force
     fi
 done
 
 # Crear contenedores
-lxc init ubuntu:20.04 "${LXC_NAME_PHP_CODEIGNITER}" --profile default
-lxc init ubuntu:20.04 "${LXC_NAME_PHP_WORDPRESS}" --profile default
-lxc init ubuntu:20.04 "${LXC_NAME_NGINX}" --profile default
-lxc init ubuntu:20.04 "${LXC_NAME_MYSQL}" --profile default
+for container in "${LXC_NAME_PHP_CODEIGNITER}" "${LXC_NAME_PHP_WORDPRESS}" "${LXC_NAME_NGINX}" "${LXC_NAME_MYSQL}"; do
+    lxc init ubuntu:20.04 "${container}" --profile default
+done
 
 # Configurar directorios
 for container in "${LXC_NAME_PHP_CODEIGNITER}" "${LXC_NAME_PHP_WORDPRESS}" "${LXC_NAME_NGINX}" "${LXC_NAME_MYSQL}"; do
-    lxc config device add "${container}" www disk source="${PWD}" path=/www
+    lxc config device add "${container}" www disk source="${WWW_PATH}" path=/www
 done
 
 # Iniciar contenedores
@@ -32,21 +33,19 @@ for container in "${LXC_NAME_PHP_CODEIGNITER}" "${LXC_NAME_PHP_WORDPRESS}" "${LX
     lxc start "${container}"
 done
 
-# Aplicar redes
-for container in "${LXC_NAME_PHP_CODEIGNITER}" "${LXC_NAME_PHP_WORDPRESS}" "${LXC_NAME_NGINX}" "${LXC_NAME_MYSQL}"; do
-    lxc exec "${container}" -- ip link add br0 type bridge
-    lxc exec "${container}" -- ip addr add 10.0.3.21/24 dev eth0 brd + scope global
-    lxc exec "${container}" -- ip route add default via 10.0.3.254 dev eth0
-    lxc exec "${container}" -- ip route add 10.0.3.0/24 via 10.0.3.254 dev eth0
-done
+# Configuración de red persistente
+if ! lxc network show mybridge >/dev/null 2>&1; then
+    lxc network create mybridge
+fi
+lxc network attach-profile mybridge default
 
 # Configurar puertos expuestos
-lxc config device add "${LXC_NAME_PHP_CODEIGNITER}" myport80 proxy listen=tcp:0.0.0.0:8000 connect=tcp:10.0.3.21:8000
-lxc config device add "${LXC_NAME_PHP_WORDPRESS}" myport80 proxy listen=tcp:0.0.0.0:8000 connect=tcp:10.0.3.22:8000
-lxc config device add "${LXC_NAME_NGINX}" myport80 proxy listen=tcp:0.0.0.0:8000 connect=tcp:10.0.3.23:8000
-lxc config device add "${LXC_NAME_MYSQL}" myport3306 proxy listen=tcp:0.0.0.0:3306 connect=tcp:10.0.3.24:3306
+lxc config device add "${LXC_NAME_PHP_CODEIGNITER}" myport80 proxy listen=tcp:0.0.0.0:8001 connect=tcp:80
+lxc config device add "${LXC_NAME_PHP_WORDPRESS}" myport80 proxy listen=tcp:0.0.0.0:8002 connect=tcp:80
+lxc config device add "${LXC_NAME_NGINX}" myport80 proxy listen=tcp:0.0.0.0:8003 connect=tcp:80
+lxc config device add "${LXC_NAME_MYSQL}" myport3306 proxy listen=tcp:0.0.0.0:3306 connect=tcp:3306
 
 # Instalación de paquetes dentro de los contenedores
-lxc exec "${LXC_NAME_NGINX}" -- apt update && apt install -y nginx
-lxc exec "${LXC_NAME_PHP_CODEIGNITER}" -- apt update && apt install -y php7.4-fpm php7.4-cli php7.4-mysql php7.4-mbstring php7.4-xml
-lxc exec "${LXC_NAME_PHP_WORDPRESS}" -- apt update && apt install -y php8.3-fpm php8.3-cli php8.3-mysql php8.3-mbstring php8.3-xml
+lxc exec "${LXC_NAME_NGINX}" -- bash -c "apt update && apt install -y nginx"
+lxc exec "${LXC_NAME_PHP_CODEIGNITER}" -- bash -c "apt update && apt install -y software-properties-common && add-apt-repository -y ppa:ondrej/php && apt update && apt install -y php7.4-fpm php7.4-cli php7.4-mysql php7.4-mbstring php7.4-xml"
+lxc exec "${LXC_NAME_PHP_WORDPRESS}" -- bash -c "apt update && apt install -y software-properties-common && add-apt-repository -y ppa:ondrej/php && apt update && apt install -y php8.3-fpm php8.3-cli php8.3-mysql php8.3-mbstring php8.3-xml"
